68fbfe34d2f6d33343c1769a6d7ca207d9afe1d6
A_XV <ahmed.walled.saad@gmail.com>
2026-02-26 06:56:30 +0400

    Add initial database of responses

---
+++ database.py
@@ -0,0 +1,8 @@
+{
+  "Hi" : "Hello What are you need ?",                                      
+  "whats your name ?" : "My name is jowa-mAi-1.6 , are you need any help ?",  
+  "How are you ?" : "I am fine , thanks for asking",                        
+  "why the bnanna crying ?" : "Because it was in peel",  
+  "what is the meaning of life ?" : "The meaning of life is 42", 
+  "bye" : "Goodbye , see you later"
+} 
